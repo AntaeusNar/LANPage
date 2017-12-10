2b79598974faa05cc1cbeab0c0975272d5626a61
AntaeusNar <jedidiah.h.wallace@gmail.com>
2017-12-10 14:57:43 -0800

    Attempted changes to flex box, removed test second menu

--- Pages/atlaspages/index.php
+++ Pages/atlaspages/index.php
@@ -2,6 +2,7 @@
 
 //Require Classes
 require_once "CLASSES/class.menu.php";
+
 //vars
 //menu array
 $mainmenu = array(
@@ -10,24 +11,16 @@ $mainmenu = array(
 	'about'=> array('text'=>'About', 'url'=>'?p=about'),
 );
 
-$topmenu = array(
-	'1' => array('text'=>'1', 'url'=>'?=1'),
-	'2' => array('text'=>'2', 'url'=>'?=2'),
-	'3' => array('text'=>'3', 'url'=>'?=3'),
-	'4' => array('text'=>'4', 'url'=>'?=4'),
-);
-
 
 //create objects
 $MainMenu = new Menu("flex-column", "nav-vert", $mainmenu);
-$TopMenu = new Menu("flex-row", "nav-hort", $topmenu);
+
 
 //load header.htm file - which loads css
 include "HTML/templates/header.htm";
 
 //call to generate mainmenu based off of the array mainmenu
 echo $MainMenu->display();
-echo $TopMenu->display();
 
 
 //load index.htm which is the main veiw content
